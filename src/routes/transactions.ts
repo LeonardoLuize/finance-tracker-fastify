03a8a00a79b1922a2503ebfa0a499d355d8ac839
Leonardo Luize <leonardo.luize2@gmail.com>
2023-03-27 11:42:14 -0300

    feat: add transactions routes

---
+++ src/routes/transactions.ts
@@ -0,0 +1,41 @@
+import { knex } from '../database'
+import crypto from 'crypto'
+import { z } from 'zod'
+import { FastifyInstance } from 'fastify'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async (req, reply) => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (req, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(req.params)
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return transaction
+  })
+
+  app.post('/', async (req, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(req.body)
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
+  })
+}
